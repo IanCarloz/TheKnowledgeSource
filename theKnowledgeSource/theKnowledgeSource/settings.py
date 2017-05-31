2b723048497ba65d897520707c8c159f08d2a85a
Cristian <cristianalemanfuentes@gmail.com>
2017-05-30 20:26:32 -0500

    Profile Done

--- theKnowledgeSource/theKnowledgeSource/settings.py
+++ theKnowledgeSource/theKnowledgeSource/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'modules.users',
+    'modules.Profile',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(os.getcwd(),'static') ]
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
